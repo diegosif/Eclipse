c609df21a721e4334afe85a5d1f41f12816dbcd6
diegosif <ag.diegos@gmail.com>
2017-10-16 16:37:46 -0500

    Ya ca ta

---
+++ semana4D1/src/semana4D1/EstructuraDesicion.java
@@ -0,0 +1,39 @@
+package semana4D1;
+
+public class EstructuraDesicion {
+
+	public EstructuraDesicion() {
+		boolean h = true;
+		boolean az = false;
+		System.out.println(h ==az);
+		System.out.println("\n\n");
+		if(h == az)
+		{			
+			System.out.println("Chana!!!");			
+		}
+		else if (h==az) 
+		{
+			System.out.println("Juana!!!");	
+		}
+		else if (h==az)
+		{
+			System.out.println("Melon!!!");	
+		}
+		else if (!h==!az)
+		{
+			System.out.println("Sandia!!!");	
+		}
+		else if(! (h==!az))
+		{
+			System.out.println("La vieja del otro dia!");	
+		}
+		else
+			System.out.println("Ni modo");	
+		
+	
+		
+		
+	
+	}
+
+}
--- semana4D1/src/semana4D1/Main.java
+++ semana4D1/src/semana4D1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		/*
 		System.out.println("hola mundo");
 		
 		int a=1;
@@ -30,9 +30,58 @@ public class Main {
 		
 		System.out.println("Chicharronera: X1= "+x1+" , X2= "+x2);
 		
+	
+		String cadena = "Parangaricutiirimicuaro";
+		
+		System.out.println("///// Cadenas ///////");
+		System.out.println("");
+		System.out.println("\n");
+		String cadenaCesar = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		int espacio =3;
+		System.out.println(cadena);
+		for(int x=0; x < cadena.length();x++)
+		{
+			System.out.println(cadena.charAt(x));
+			cadena = cadena.replace(cadena.charAt(x), cadenaCesar.charAt( x + espacio));
+		
+		System.out.println(cadena);
+		////subcadenas
+		System.out.println(cadena.substring(3,6));
+		//convertir a minusculas
+		System.out.println(cadena.toLowerCase());
+		
+		///Array
+		String [] meses ={"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Ago", "Sep", "oct", "Nov","Dic"};
+		String []  sem= {"mon","Tue","wed","Thu","Fri","sat","Sun"};
+		
+		//objetivo: mostrar: enero, julio, noviembre, diciembre y marzo en la misma linea
+		
+		System.out.print(meses[0]+" " +meses[6]+" " +meses[10]+" " +meses[11]+" " +meses[2]+" " +sem[0]+" " +sem[4]+" " +sem[1]);
+		
+		//Fallo provocado
+		//System.out.println(sem[8]);
+ 		
+		boolean h = true;
+		boolean az = false;
+		System.out.println("\n\n");
+		System.out.println(!h == !az);
+		System.out.println("\n\n");
+		if(h == az);
+		{
+			
+		}
+		
+			
 		
 		
+
 		
-		
+		}*/
+		EstructuraDesicion d=new EstructuraDesicion();
+				
 	}
 }
+
+
+
+
